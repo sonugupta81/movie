04fae96eb0a037fcd58d5732f5aea8179dfbabc2
sonugupta81 <sonuguptak2005@gmail.com>
2025-12-23 11:12:43 +0530

    px

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -4,7 +4,7 @@ import { Search } from 'lucide-react';
 
 const home =()=> {
   return (
-    <div className='flex min:h-screen w-screen item-center justify-center pt-40 bg-blue-300 pb-10 '>
+    <div className='flex min:h-screen w-screen item-center justify-center pt-8 md:pt-40 bg-blue-300 pb-10 px-4 '>
       <div className='flex  w-90 md:w-300 rounded-2xl bg-white border border-green-900'>
         <input type="text" placeholder='Search for movies, series, anime...' className='w-full border-none bg-transparent px-4 py-1 text-gray-500 outline-none'/>
         <button className='m-3 rounded-4xl bg-teal-800 px-4 md:px-8 md:py-4 py-2 text-white hover:bg-teal-700 hover:cursor-pointer'>Search</button>
